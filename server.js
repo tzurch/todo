5a465318914d54b7d323413bd2e96489a887ed93
Thomas Zurch <zurch@appstoremedia.com>
2016-09-21 17:00:22 +0000

    Add /todos and /todos:id

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-app.get('/', function (req, res) {
-    res.send('ToDo API Root');  
-  });
-  
-  app.listen(PORT, function () {
-      console.log('Express Listening on port' + PORT + '!');
+    var express = require("express");
+    var app = express();
+    var PORT = process.env.PORT || 3000;
+    var todos =[{
+        id: 1,
+        description: 'Meet Dad for lunch',
+        completed: false
+    }, {
+        id: 2,
+        description: 'Go to market',
+        completed: false
     
-  });
+    }, {
+        id: 3,
+        description: 'Interview with Intuit',
+        completed: true   
+        
+    }];
+    
+    app.get('/', function (req, res) {
+        res.send('ToDo API Root');  
+      });
+     // GET /todos
+    app.get('/todos', function (req, res) {
+        res.json(todos);
+    });
+    
+    
+    // GET /todos/:id  
+    app.get('/todos/:id', function (req, res) {
+        var todoId = parseInt(req.params.id, 10);
+        var matchedTodo;
+        
+        todos.forEach(function (todo) {
+            if (todoId === todo.id) {
+            matchedTodo = todo;
+            }
+        });
+    
+        if (matchedTodo) {
+            res.json(matchedTodo);
+        } else {
+            res.status(404). send();
+        }
+    
+    });
+    
+    app.listen(PORT, function () {
+          console.log('Express Listening on port' + PORT + '!');
+       
+      });
+      
+      
