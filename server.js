08b9e4080af326e90b9879874aab0586647b1eb9
Thomas Zurch <zurch@appstoremedia.com>
2016-09-23 02:30:52 +0000

    refactore API with underscore

--- server.js
+++ server.js
@@ -1,58 +1,70 @@
-    var express = require("express");
+    var express = require('express');
     var bodyParser = require('body-parser');
+    var _ = require('underscore');
     
     var app = express();
     var PORT = process.env.PORT || 3000;
-    var todos =[];
+    var todos = [];
     var todoNextId = 1;
     
-    app.use(bodyParser.json())
+    app.use(bodyParser.json());
     
     app.get('/', function (req, res) {
-        res.send('ToDo API Root');  
-      });
-     // GET /todos
-    app.get('/todos', function (req, res) {
-        res.json(todos);
+    	res.send('Todo API Root');
     });
     
+    // GET /todos
+    app.get('/todos', function (req, res) {
+    	res.json(todos);
+    });
     
-    // GET /todos/:id  
+    // GET /todos/:id
     app.get('/todos/:id', function (req, res) {
-        var todoId = parseInt(req.params.id, 10);
-        var matchedTodo;
-        
-        todos.forEach(function (todo) {
-            if (todoId === todo.id) {
-            matchedTodo = todo;
-            }
-        });
+    	var todoId = parseInt(req.params.id, 10);
+    	var matchedTodo = _.findWhere(todos, {id: todoId});
     
-        if (matchedTodo) {
-            res.json(matchedTodo);
-        } else {
-            res.status(404). send();
-        }
-    
+    	if (matchedTodo) {
+    		res.json(matchedTodo);
+    	} else {
+    		res.status(404).send();
+    	}
     });
     
     // POST /todos
     app.post('/todos', function (req, res) {
-        var body = req.body;
-        
-    // add id field
-        body.id = todoNextId++;
+    	var body = _.pick(req.body, 'description', 'completed');
     
-    //push body into array    
-        todos.push(body);
-        
-        res.json(body);
-        
+    	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    		return res.status(400).send();
+    	}
+    
+    	body.description = body.description.trim();	
+    	body.id = todoNextId++;
+    
+    	todos.push(body);
+    	
+    	res.json(body);
     });
+    
     
     app.listen(PORT, function () {
-          console.log('Express Listening on port' + PORT + '!');
-       
+    	console.log('Express listening on port ' + PORT + '!');
     });
-      
-      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
